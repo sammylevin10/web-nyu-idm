5bb4d1f7eaa4101c9d4e17ced2b4111ba59b429c
sammy01px2019 <sammylevin@MacBook-Pro-SL.local>
2020-04-28 15:43:26 -0400

    Italicized animation

--- portfolio-site/script.js
+++ portfolio-site/script.js
@@ -6,4 +6,42 @@ $(document).ready(function() {
         this.scrollLeft -= (delta * 0.5);
         e.preventDefault();
     });
+
 });
+
+
+//JS SMOOTH ANCHORS
+// $("a[href^='#']").click(function(e) {
+//   e.preventDefault();
+//   console.log("invoked");
+//
+//   var position = $($(this).attr("href")).offset().top;
+//   console.log(position);
+//   console.log($($(this).attr("href")));
+//
+//   $("body, html").animate({
+//     scrollTop: position
+//   } /* speed */ );
+// });
+// Add smooth scrolling to all links
+// $("a").on('click', function(event) {
+//
+//   // Make sure this.hash has a value before overriding default behavior
+//   if (this.hash !== "") {
+//     // Prevent default anchor click behavior
+//     event.preventDefault();
+//
+//     // Store hash
+//     var hash = this.hash;
+//
+//     // Using jQuery's animate() method to add smooth page scroll
+//     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//     $('html, body').animate({
+//       scrollTop: $(hash).offset().top
+//     }, 800, function(){
+//
+//       // Add hash (#) to URL when done scrolling (default click behavior)
+//       window.location.hash = hash;
+//     });
+//   } // End if
+// });
